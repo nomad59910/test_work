1871c292216885a86e48cacd2639411f4794a4c1
nomad59910 <nomad59910@users.github.com>
2017-08-10 03:20:48 +0400

    добавлен пользовательский интерфейс изменение

--- src/static/js/todo.js
+++ src/static/js/todo.js
@@ -15,14 +15,14 @@ function getTask(url){
     return resultData;
 }
 
-
 var Todos = Vue.extend({
     name: 'todos'
 });
 
-Vue.component('modal', {
+Vue.component('add-task-modal', {
+    delimiters: ["[", "]"],
     template: '#modal-template',
-    props: ['show'],
+    props: ['show', 'title'],
     data: function () {
         return {
             name: "",
@@ -49,30 +49,60 @@ Vue.component('modal', {
     }
 });
 
+Vue.component('edit-task-modal', {
+    delimiters: ["[", "]"],
+    template: '#modal-template',
+    props: ['show', 'name_', 'description_', 'title'],
+    data: function () {
+        return {
+            name: this.name_,
+            description: this.description_,
+        }
+    },
+    methods: {
+        savePost: function () {
+            this.$emit('save-data', {   });
+            this.close();
+        },
+        close: function () {
+            this.$emit('hide-modal');
+        }
+    },
+    watch : {
+        name_ : function () {
+            this.name = this.name_;
+        },
+        description_ : function () {
+            this.description = this.description_;
+        }
+    },
+    mounted: function () {
+        document.addEventListener("keydown", (e) => {
+            if (this.show && e.keyCode == 27) {
+                this.close();
+            }
+        });
+    }
+});
+
 var vm = new Vue({
     delimiters: ["[", "]"],
     el : '#todos',
     data : {
-        showModal: false,
+        showAddTaskModal: false,
+        showEditTaskModal: false,
+        editingTaskName: "",
+        editingTaskDescription: "",
         tasks : [],
         isLoadind : true,
         next: undefined,
     },
-    mounted : function(){
-        var tasks = getTask(urlLoadTask);
-        this.tasks = tasks["results"];
-        this.next = tasks["next"];
-        this.isLoadind = false;
-    },
-    created () {
-        $(window).scroll(this.handleScroll);
-    },
     methods: {
-        hideModal: function(){
-            this.showModal = false;
+        changeTask: function(data){
+            // this.tasks[this.editingTaskId].name = name;
+            // this.tasks[this.editingTaskId].description = description;
         },
         addTask: function(name, description){
-            console.log(this.newTask);
             var newTask = Object.assign({}, this.newTask);
             this.tasks.unshift({
                 "name": name,
@@ -86,11 +116,15 @@ var vm = new Vue({
             this.tasks.splice(index, 1);
         },
         changeState : function(index){
-            console.log(this.tasks[index]);
             this.tasks[index].is_done = !this.tasks[index].is_done;
         },
+        openChangeTaskModal: function(index){
+            this.editingTaskName = this.tasks[index].name;
+            this.editingTaskDescription = this.tasks[index].description;
+            this.showEditTaskModal = true;
+        },
         handleScroll : function(e) {
-    		if ($(document).height() - $(window).height() == $(window).scrollTop() && !this.isLoadind) {
+    		if (($(document).height() - $(window).height()) - 20 < $(window).scrollTop() && !this.isLoadind) {
                 this.isLoadind = true;
                 if (this.next) {
                     var tasks = getTask(this.next);
@@ -103,5 +137,14 @@ var vm = new Vue({
                 }
     		}
         }
+    },
+    mounted : function(){
+        var tasks = getTask(urlLoadTask);
+        this.tasks = tasks["results"];
+        this.next = tasks["next"];
+        this.isLoadind = false;
+    },
+    created () {
+        $(window).scroll(this.handleScroll);
     },
 });
