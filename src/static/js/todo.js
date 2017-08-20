dc2e2f707c32a724a60103ea5b3a7254208be95e
nomad59910 <nomad59910@users.github.com>
2017-08-20 14:57:32 +0400

    связал api с frontend

--- src/static/js/todo.js
+++ src/static/js/todo.js
@@ -1,10 +1,16 @@
-var urlLoadTask = "/task/api/tasks-list?limit=13";
+var urlLoadTask = "/api/task/list/";
 
-function getTask(url){
+function getTask(url, params){
     var resultData = undefined;
+
+    if (!params) {
+        var params = {};
+    }
+
     $.get({
         type: "GET",
         url: url,
+        data: params,
         success: function(data){
             resultData = data;
         },
@@ -27,17 +33,47 @@ Vue.component('add-task-modal', {
         return {
             name: "",
             description: "",
+            error: {
+                name : '',
+                description : '',
+            }
         }
     },
     methods: {
-        savePost: function () {
-            this.$emit('save-data', this.name, this.description);
-            this.close();
+        saveTask: function () {
+            params = {
+                "name": this.name,
+                "description": this.description,
+            }
+
+            var that = this;
+            $.post({
+                dataType: 'json',
+                url: '/api/task/add/',
+                data: params,
+                success: function(data){
+                    that.$emit('save-data', data);
+                    that.close();
+                },
+                error: function(error) {
+                    errorJson = error.responseJSON;
+                    for (var prop in errorJson) {
+                        if(!errorJson.hasOwnProperty(prop)) continue;
+                        that.error[prop] = errorJson[prop].join(' ');
+                    }
+                },
+                dataType: "JSON",
+                async: false,
+            });
         },
         close: function () {
             this.$emit('hide-modal');
             this.name = "";
             this.description = "";
+            this.error = {
+                name : '',
+                description : '',
+            };
         }
     },
     mounted: function () {
@@ -52,29 +88,59 @@ Vue.component('add-task-modal', {
 Vue.component('edit-task-modal', {
     delimiters: ["[", "]"],
     template: '#modal-template',
-    props: ['show', 'name_', 'description_', 'title'],
+    props: ['show', 'task', 'title'],
     data: function () {
         return {
-            name: this.name_,
-            description: this.description_,
+            name: "",
+            description: "",
+            error: {
+                name : '',
+                description : '',
+            }
         }
     },
     methods: {
-        savePost: function () {
-            this.$emit('save-data', {   });
-            this.close();
+        saveTask: function () {
+            var url = '/api/task/edit/' + this.task.id + '/'
+            params = {
+                name: this.name,
+                description: this.description,
+            }
+
+            var that = this;
+            $.ajax({
+                type: "PUT",
+                dataType: 'json',
+                url: url,
+                data: params,
+                success: function(data){
+                    that.$emit('save-data', data, that.task.id);
+                    that.close();
+                },
+                error: function(error) {
+                    errorJson = error.responseJSON;
+                    for (var prop in errorJson) {
+                        if(!errorJson.hasOwnProperty(prop)) continue;
+                        that.error[prop] = errorJson[prop].join(' ');
+                    }
+                },
+                dataType: "JSON",
+                async: false,
+            });
         },
         close: function () {
             this.$emit('hide-modal');
         }
     },
     watch : {
-        name_ : function () {
-            this.name = this.name_;
+        show : function () {
+            this.name = this.task.name;
+            this.description = this.task.description;
+            this.error = {
+                name : '',
+                description : '',
+            };
         },
-        description_ : function () {
-            this.description = this.description_;
-        }
     },
     mounted: function () {
         document.addEventListener("keydown", (e) => {
@@ -91,58 +157,109 @@ var vm = new Vue({
     data : {
         showAddTaskModal: false,
         showEditTaskModal: false,
-        editingTaskName: "",
-        editingTaskDescription: "",
+        editingTask: "",
         tasks : [],
         isLoadind : true,
-        next: undefined,
+        isFilter: false,
     },
     methods: {
-        changeTask: function(data){
-            // this.tasks[this.editingTaskId].name = name;
-            // this.tasks[this.editingTaskId].description = description;
+        changeTask: function(data, id){
+            var taskId = this.tasks.findIndex(task => task.id == id);
+            for (var prop in data) {
+                if(!data.hasOwnProperty(prop)) continue;
+                this.tasks[taskId][prop] = data[prop]
+            }
         },
-        addTask: function(name, description){
-            var newTask = Object.assign({}, this.newTask);
-            this.tasks.unshift({
-                "name": name,
-                "description": description,
-                "user_added": "admin",
-                "date_created": '-',
-                "is_done": false
-            });
+        addTask: function(newTask){
+            this.tasks.unshift(newTask);
         },
         removeTask : function(index){
-            this.tasks.splice(index, 1);
+            var idTask =  this.tasks[index].id
+            var url =  '/api/task/delete/' + idTask + '/'
+            var that = this;
+            $.ajax({
+                type: "DELETE",
+                dataType: 'json',
+                url: url,
+                success: function(data){
+                    that.tasks.splice(index, 1);
+                },
+                error: function(error) {
+                    errorJson = error.responseJSON;
+                    console.log(errorJson);
+                },
+                dataType: "JSON",
+                async: false,
+            });
         },
         changeState : function(index){
-            this.tasks[index].is_done = !this.tasks[index].is_done;
+            var that = this;
+            if (!this.tasks[index].is_done) {
+                $.ajax({
+                    type: "PUT",
+                    dataType: 'json',
+                    url: '/api/task/done/' + this.tasks[index].id + '/',
+                    success: function(data){
+                        that.tasks[index].is_done = data.is_done;
+                    },
+                    error: function(error) {
+                        errorJson = error.responseJSON;
+                        console.log(errorJson);
+                    },
+                    dataType: "JSON",
+                    async: false,
+                });
+            }
+            else{
+                $.ajax({
+                    type: "PUT",
+                    dataType: 'json',
+                    url: '/api/task/undone/' + this.tasks[index].id + '/',
+                    success: function(data){
+                        that.tasks[index].is_done = data.is_done;
+                    },
+                    error: function(error) {
+                        errorJson = error.responseJSON;
+                        console.log(errorJson);
+                    },
+                    dataType: "JSON",
+                    async: false,
+                });
+            }
         },
         openChangeTaskModal: function(index){
-            this.editingTaskName = this.tasks[index].name;
-            this.editingTaskDescription = this.tasks[index].description;
+            this.editingTask = this.tasks[index];
             this.showEditTaskModal = true;
         },
         handleScroll : function(e) {
     		if (($(document).height() - $(window).height()) - 20 < $(window).scrollTop() && !this.isLoadind) {
                 this.isLoadind = true;
-                if (this.next) {
-                    var tasks = getTask(this.next);
-                    this.tasks = this.tasks.concat(tasks["results"]);
-                    this.next = tasks["next"];
-                    this.isLoadind = false;
-                }
-                else{
-                    this.isLoadind = false;
-                }
+
+                var params = {}
+                var params = {limit: 10}
+                if (this.isFilter) params["done"] = "False";
+                params["prev_id"] = this.tasks[this.tasks.length - 1].id;
+                var tasks = getTask(urlLoadTask, params);
+
+                this.tasks = this.tasks.concat(tasks);
+                this.isLoadind = false;
     		}
-        }
+        },
+        loadTasks: function(){
+            this.tasks = [];
+            this.isLoadind = true;
+
+            var params = {limit: 10}
+            if (this.isFilter) params["done"] = "False";
+
+            var tasks = getTask(urlLoadTask, params);
+
+            this.tasks = tasks;
+            this.isLoadind = false;
+        },
     },
     mounted : function(){
-        var tasks = getTask(urlLoadTask);
-        this.tasks = tasks["results"];
-        this.next = tasks["next"];
-        this.isLoadind = false;
+        this.loadTasks();
     },
     created () {
         $(window).scroll(this.handleScroll);
--- src/task/views.py
+++ src/task/views.py
@@ -2,18 +2,9 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.views import View
 
-# Create your views here.
 
 class TaskListView(View):
     template_name = 'task/tasks.html'
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
-
-    # def post(self, request, *args, **kwargs):
-    #     form = self.form_class(request.POST)
-    #     if form.is_valid():
-    #         # <process form cleaned data>
-    #         return HttpResponseRedirect('/success/')
-    #
-    #     return render(request, self.template_name, {'form': form})
